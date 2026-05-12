a3bb1eb4c9e33cf94d0c5e6e42180656f156c8c3
Sanika Bhawale <sanikabhawale23@gmail.com>
2026-05-12 22:49:13 +0530

    Added Chatbot Code

---
+++ codes/Chatbot.py
@@ -0,0 +1,30 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+
+# chatbot questions and answers
+pairs = [
+
+    [r"hi|hello|hey",
+     ["Hello! Welcome to Ice Cream Shop "]],
+
+    [r"what flavors do you have ?",
+     ["We have chocolate, vanilla and strawberry."]],
+
+    [r"price",
+     ["Ice cream starts from 50 rupees."]],
+
+    [r"timing",
+     ["Shop is open from 10 AM to 11 PM."]],
+
+    [r"bye",
+     ["Bye! Visit again "]]
+]
+
+# create chatbot
+chatbot = Chat(pairs, reflections)
+
+print("Ice Cream Shop Chatbot")
+print("Type 'bye' to exit\n")
+
+# start chat
+chatbot.converse()
